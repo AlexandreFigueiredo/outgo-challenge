a038714b18e288191fe7ff6e1a75d5ebedf96390
Alexandre Figueiredo <ale.fig1701@gmail.com>
2022-09-14 23:29:43 -0300

    Buildind home structures

--- src/components/NowShowing/index.tsx
+++ src/components/NowShowing/index.tsx
@@ -4,6 +4,41 @@ export function NowShowing() {
     return (
         <div className={styles.nowShowingContainer}>
             <h2 className={styles.nowShowingTitle}>Now Showing</h2>
+
+            <div className={styles.nowShowingList}>
+                <div className={styles.carouselCard}>
+                    <div className={styles.cardImg}></div>
+                    <h3 className={styles.cardTitle}>Spiderman: No way home</h3>
+                    <div className={styles.cardRating}>
+                        <img className={styles.cardRatingIcon} src="/images/icon-star.png" alt="Estrela" />
+                        <span>9.3/10</span>
+                    </div>
+                </div>
+                <div className={styles.carouselCard}>
+                    <div className={styles.cardImg}></div>
+                    <h3 className={styles.cardTitle}>Spiderman: No way home</h3>
+                    <div className={styles.cardRating}>
+                        <img className={styles.cardRatingIcon} src="/images/icon-star.png" alt="Estrela" />
+                        <span>9.3/10</span>
+                    </div>
+                </div>
+                <div className={styles.carouselCard}>
+                    <div className={styles.cardImg}></div>
+                    <h3 className={styles.cardTitle}>Spiderman: No way home</h3>
+                    <div className={styles.cardRating}>
+                        <img className={styles.cardRatingIcon} src="/images/icon-star.png" alt="Estrela" />
+                        <span>9.3/10</span>
+                    </div>
+                </div>
+                <div className={styles.carouselCard}>
+                    <div className={styles.cardImg}></div>
+                    <h3 className={styles.cardTitle}>Spiderman: No way home</h3>
+                    <div className={styles.cardRating}>
+                        <img className={styles.cardRatingIcon} src="/images/icon-star.png" alt="Estrela" />
+                        <span>9.3/10</span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/Popular/index.tsx
+++ src/components/Popular/index.tsx
@@ -4,6 +4,28 @@ export function Popular() {
     return (
         <div className={styles.popularContainer}>
             <h2 className={styles.popularTitle}>Popular</h2>
+
+            <div className={styles.popularList}>
+                <div className={styles.popularCard}>
+                    <div className={styles.cardImg}></div>
+                    <div className={styles.cardContent}>
+                        <h3 className={styles.cardTitle}>Venom Let There Be Carnage</h3>
+                        <div className={styles.cardRating}>
+                            <img className={styles.cardRatingIcon} src="/images/icon-star.png" alt="Estrela" />
+                            <span>9.3/10</span>
+                        </div>
+                        <div className={styles.cardTags}>
+                            <span className={styles.tags}>Horror</span>
+                            <span className={styles.tags}>Mistey</span>
+                            <span className={styles.tags}>Thriller</span>
+                        </div>
+                        <div className={styles.cardDuration}>
+                            <img className={styles.cardDurationIcon} src="/images/icon-duration.png" alt="Duração" />
+                            <span>1h 47m</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
